8a794df82c0baa8727a2c108c944fc33bb9b6bc7
sivasuthan manojan <mjcode6@gmail.com>
2023-09-24 16:58:01 +0200

    i work on angular forms and going really well

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { EmpInfoComponent } from './emp-info/emp-info.component';
+import { FormComponentComponent } from './form-component/form-component.component';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmpInfoComponent
+    FormComponentComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
